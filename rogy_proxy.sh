c8bfd994ac658a3d6648520c665ad3218f280f04
jiko <jiko.rogy@gmail.com>
2014-12-21 23:35:50 +0900

    first commit

--- rogy_proxy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export http_proxy="192.168.11.92:8008"
-export HTTP_PROXY="192.168.11.92:8008"
-export https_proxy=$http_proxy
-export ftp_proxy=$http_proxy
-export rsync_proxy=$http_proxy
-export no_proxy="localhost,127.0.0.1"
-git config --global http.proxy 192.168.11.92:8008
-git config --global https.proxy 192.168.11.92:8008
